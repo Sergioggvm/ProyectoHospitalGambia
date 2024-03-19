b90c050c210c6f0c30943993ffe6b39fcf96026a
Mario Muñoz <snakeyes271101@gmail.com>
2024-03-19 19:02:39 +0100

    Cambiadas imagenes y añadidos fragments

---
+++ app/src/main/java/com/example/proyectohospitalgambia/IntroducirMoodAndEnergyViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.proyectohospitalgambia
+
+import androidx.lifecycle.ViewModel
+
+class IntroducirMoodAndEnergyViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/proyectohospitalgambia/feature/vistaIntroducirSleep/IntroducirSleepViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.proyectohospitalgambia.feature.vistaIntroducirSleep
+
+import androidx.lifecycle.ViewModel
+
+class IntroducirSleepViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
