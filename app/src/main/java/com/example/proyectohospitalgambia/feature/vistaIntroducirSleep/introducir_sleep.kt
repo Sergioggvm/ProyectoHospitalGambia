27f56c34f86bf7b25fae9fc350ed90c388e4d03e
Mario Muñoz <snakeyes271101@gmail.com>
2024-03-25 11:37:38 +0100

    Spinner sueño funciona

--- app/src/main/java/com/example/proyectohospitalgambia/feature/vistaIntroducirSleep/introducir_sleep.kt
+++ app/src/main/java/com/example/proyectohospitalgambia/feature/vistaIntroducirSleep/introducir_sleep.kt
@@ -7,8 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.proyectohospitalgambia.R
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
-class introducir_sleep : Fragment() {
+class introducir_sleep : Fragment(), AdapterView.OnItemSelectedListener {
+
+    lateinit var spinnerSleepQuality: Spinner
+
 
     companion object {
         fun newInstance() = introducir_sleep()
@@ -26,6 +32,32 @@ class introducir_sleep : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_introducir_sleep, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_introducir_sleep, container, false)
+
+        spinnerSleepQuality = view.findViewById(R.id.spinner_sleepQuality)
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.calidad_suenno, // Define este array en tu archivo strings.xml
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerSleepQuality.adapter = adapter
+        }
+
+        spinnerSleepQuality.onItemSelectedListener = this
+
+        return view
+
+        //return inflater.inflate(R.layout.fragment_introducir_sleep, container, false)
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        // Acciones cuando se selecciona un ítem del spinner
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        // Acciones cuando no se selecciona ningún ítem del spinner
     }
 }
